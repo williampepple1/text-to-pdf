9b2570e370820cd2878536418c03e93b0eba5580
William Pepple <williampepple1@gmail.com>
2023-10-06 14:35:04 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,3 +5,15 @@ import (
 
 	"github.com/jung-kurt/gofpdf"
 )
+
+func main() {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.MoveTo(0, 10)
+	pdf.Cell(1, 1, "Hello world")
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err == nil {
+		fmt.Println("PDF generated successfully")
+	}
+}
