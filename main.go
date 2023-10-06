9916457bfc659dcee9f75437cfe59b7123d3e4f1
William Pepple <williampepple1@gmail.com>
2023-10-06 14:36:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/jung-kurt/gofpdf"
 )
 
 func main() {
+	text, err := ioutil.ReadFile("lorem.txt")
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
 	pdf.MoveTo(0, 10)
-	pdf.Cell(1, 1, "Hello world")
-	err := pdf.OutputFileAndClose("hello.pdf")
+	pdf.Cell(1, 1, "Lorem Ipsum")
+	pdf.MoveTo(0, 20)
+	pdf.SetFont("Arial", "", 14)
+	width, _ := pdf.GetPageSize()
+	pdf.MultiCell(width, 10, string(text), "", "", false)
+	err = pdf.OutputFileAndClose("hello.pdf")
 	if err == nil {
 		fmt.Println("PDF generated successfully")
 	}
